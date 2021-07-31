8e04ca706b221da90498317779eb4b7222f52950
Anay Wadhera <awadhera@berkeley.edu>
2021-07-30 22:58:11 -0700

    hardware/oneplus: add touch handler for offscreen gestures
    Change-Id: Ief28f7eccc3946d134c0da1f26de6eac4afe557e

---
+++ TouchHandler/src/com/statix/touchhandler/TouchscreenGestureActivity.java
@@ -0,0 +1,25 @@
+package com.statix.touchhandler;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.MenuItem;
+
+import androidx.preference.Preference;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class TouchscreenGestureActivity extends CollapsingToolbarBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            TouchscreenGestureSettings settings = new TouchscreenGestureSettings();
+            getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, settings)
+                .commit();
+        }
+    }
+}
