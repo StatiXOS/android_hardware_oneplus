268ea0a19c5b54b5fd02b498554b1a5421ac8f82
LuK1337 <priv.luk@gmail.com>
2022-02-13 20:09:15 +0100

    fingerprint: Add support for TARGET_USES_FOD_ZPOS
    [mvaisakh: Use AOSP Soong conditionals, instead of generated_kernel_headers]
    Change-Id: Iaf79d5393169b986197be058ac2166d2e448dfa0

--- hidl/fingerprint/UdfpsExtension.cpp
+++ hidl/fingerprint/UdfpsExtension.cpp
@@ -6,7 +6,11 @@
 #include <compositionengine/UdfpsExtension.h>
 
 uint32_t getUdfpsZOrder(uint32_t z, bool touched) {
+#ifdef FOD_PRESSED_LAYER_ZORDER
+    return touched ? z | FOD_PRESSED_LAYER_ZORDER : z;
+#else
     return touched ? 0xfc8 : z;
+#endif
 }
 
 uint64_t getUdfpsUsageBits(uint64_t usageBits, bool) {
